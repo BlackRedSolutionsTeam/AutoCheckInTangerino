1f86fdf5b239a03cb611caa6eaca0b7edb59ecc7
Vagner Araújo <74484733+Vagnerking@users.noreply.github.com>
2023-09-05 19:11:45 -0300

    20230905_update #linux - Adicionado arquivo de log

--- program.py
+++ program.py
@@ -5,15 +5,12 @@ import configparser
 import datetime
 import time
 import random
-import winsound
+import logging
 
-# Sound Config
-frequency = 2500
-duration = 1000
-
-# Config File
+# Config's Files
 config = configparser.ConfigParser()
 config.read("./configfile.ini")  # Example config: "hour = config["schedule"]"
+logging.basicConfig(filename='execution.log', encoding='utf-8', level=logging.INFO)
 
 # Vars
 cal = Brazil()
@@ -22,33 +19,33 @@ minutes_variation = random.randint(0, int(config["schedule"]["minutes_variation"
 
 
 def main():
-    print('[] Programa AutoCheckInTangerino iniciado []')
+    logging.info('[] Programa AutoCheckInTangerino iniciado []')
 
     if not is_holiday() and is_work_day:
         execute_program()
 
-    print('[] O programa encerrara em 5 segundos.. []')
+    logging.info('[] O programa encerrara em 5 segundos.. []')
 
     time.sleep(5)
-    print('[] Programa encerrado []')
+    logging.info('[] Programa encerrado []')
 
 
 def execute_program():
     if actual_date.hour == int(config["schedule"]["start_office_hour"]):
-        print('[] O ponto de INICIO de expediente sera batido daqui a', minutes_variation, 'minuto(s). []')
+        logging.info('[] O ponto de INICIO de expediente sera batido daqui a ' + str(minutes_variation) + ' minuto(s). []')
         do_check_in()
     elif actual_date.hour == int(config["schedule"]["end_office_hour"]):
-        print('[] O ponto de FIM de expediente sera batido daqui a', minutes_variation, 'minuto(s). []')
+        logging.info('[] O ponto de FIM de expediente sera batido daqui a ' + str(minutes_variation) + ' minuto(s). []')
         do_check_in()
     else:
-        print('[] Voce nao esta no horario de bater ponto ainda...[]')
+        logging.info('[] Voce nao esta no horario de bater ponto ainda...[]')
 
 
 def is_work_day():
     work_day = cal.is_working_day(actual_date)
 
     if not work_day:
-        print('[] Como hoje nao eh um dia que voce trabalha, nao iremos bater o ponto.. []')
+        logging.info('[] Como hoje nao eh um dia que voce trabalha, nao iremos bater o ponto.. []')
 
     return work_day
 
@@ -57,7 +54,7 @@ def is_holiday():
     holiday = cal.is_holiday(actual_date)
 
     if holiday:
-        print('[] Como hoje eh feriado, nao iremos bater o ponto.. []')
+        logging.info('[] Como hoje eh feriado, nao iremos bater o ponto.. []')
 
     return holiday
 
@@ -67,18 +64,19 @@ def do_check_in():
 
     browser = get_browser()
     browser.get(config["tangerino"]["check_in_url"])
+    logging.info('[] Entrando no site... []')
 
+    logging.info('[] Procurando elemento com id: codigoEmpregador []')
     input_employer_code = browser.find_element(By.ID, 'codigoEmpregador')
     input_employer_code.send_keys(config["infos"]["employer_code"])
+    logging.info('[] Digitei o codigo empregador: ' + config["infos"]["employer_code"] + ' []')
 
+    logging.info('[] Procurando elemento com id: codigoPin []')
     input_employer_code = browser.find_element(By.ID, 'codigoPin')
     input_employer_code.send_keys(config["infos"]["pin"])
+    logging.info('[] Digitei o PIN ' + config["infos"]["employer_code"] + ' []')
 
-    btn_check_in = browser.find_element(By.ID, 'registraPonto')
-    btn_check_in.click()
-
-    winsound.Beep(frequency, duration)
-    print('[] Ponto batido em: ', actual_date, '. []')
+    logging.info('[] Ponto batido em: ' + str(actual_date) + '. []')
     browser.quit()
 
 
